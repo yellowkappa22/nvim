2227bb68a11d7b9a298ccc289dd79c15025ff924
Kasper Pettersson <165212445+yellowkappa22@users.noreply.github.com>
2025-12-09 14:12:25 -0500

    Remove image.nvim dependency and update config
    Removed dependency on '3rd/image.nvim' and updated molten image provider configuration.

--- lua/plugins/molten-nvim.lua
+++ lua/plugins/molten-nvim.lua
@@ -1,12 +1,10 @@
 return {
   "benlubas/molten-nvim",
   version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
-  dependencies = { "3rd/image.nvim" },
   build = ":UpdateRemotePlugins",
 
   config = function()
     -- Molten options
-    vim.g.molten_image_provider = "image.nvim"
     vim.g.molten_output_win_max_height = 20
   end,
 }
